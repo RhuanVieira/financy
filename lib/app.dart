abd53b3c8ee704f585545c60cdedc3f06bf7be42
rhuan vieira da silva santos <rhuan@DSN1191348100.edu119.local>
2026-03-26 15:15:53 -0300

    adições de childrens e titulo

--- lib/app.dart
+++ lib/app.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class Financy extends StatelessWidget {
   const Financy({super.key});
@@ -7,13 +8,33 @@ class Financy extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        backgroundColor: Colors.amber,
+        toolbarHeight: 100,
+        backgroundColor: Colors.red,
+        title: Text.rich(
+          TextSpan(
+            children: [
+              TextSpan(
+                text: "BANK THH\n",
+                style: GoogleFonts.montserrat(
+                  color: Colors.white,
+                  fontSize: 30,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              TextSpan(
+                text: "Tudo o que você precisa em um só\n aplicativo",
+                style: GoogleFonts.montserrat(fontSize: 15, color: Colors.white),
+              ),
+            ],
+          ),
+        ),
       ),
-      drawer: const Drawer(
-        backgroundColor: Color.fromARGB(255, 243, 243, 243),
-        
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        backgroundColor: const Color.fromARGB(255, 255, 7, 7),
+        child: const Icon(Icons.pix, color: Colors.white),
+        focusColor: const Color.fromARGB(255, 0, 238, 255),
       ),
-      floatingActionButton: FloatingActionButton(onPressed: (){}, backgroundColor: Colors.amber, child: const Icon(Icons.add),),
     );
   }
 }
